ce345b1bd3cb94d7213dc6db80a43d7eb220997a
PedroLzr <cereus992@gmail.com>
2020-01-22 16:55:33 +0100

    Tercer commit

--- js/mapa.js
+++ js/mapa.js
@@ -1,7 +1,25 @@
 let imgMapa;
+let data = [];
+let ciudades = array("http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=2a561310e42c062d6e1109fc958905c6&zip=28001,es&units=metric",
+                    "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=2a561310e42c062d6e1109fc958905c6&zip=03501,es&units=metric",
+                    "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=2a561310e42c062d6e1109fc958905c6&zip=03501,es&units=metric",
+                    "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=2a561310e42c062d6e1109fc958905c6&zip=03501,es&units=metric",
+                    "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=2a561310e42c062d6e1109fc958905c6&zip=03501,es&units=metric",
+                    "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=2a561310e42c062d6e1109fc958905c6&zip=03501,es&units=metric",
+                    "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=2a561310e42c062d6e1109fc958905c6&zip=03501,es&units=metric",
+                    "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=2a561310e42c062d6e1109fc958905c6&zip=03501,es&units=metric",
+                    "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=2a561310e42c062d6e1109fc958905c6&zip=03501,es&units=metric",
+                    "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=2a561310e42c062d6e1109fc958905c6&zip=03501,es&units=metric",
+                    "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=2a561310e42c062d6e1109fc958905c6&zip=03501,es&units=metric",
+                    "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=2a561310e42c062d6e1109fc958905c6&zip=03501,es&units=metric",
+                    "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=2a561310e42c062d6e1109fc958905c6&zip=03501,es&units=metric",
+                    "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=2a561310e42c062d6e1109fc958905c6&zip=03501,es&units=metric");
 
 function preload(){
     imgMapa = loadImage('assets/World-Map.svg');
+    for(let i = 0; i<ciudades.length; i++){
+        data.push(loadJSON(ciudades[i]));
+    }
 }
 
 function setup(){
@@ -12,13 +30,33 @@ function setup(){
 
 function draw(){
     pintarCuadricula();
+    pintarTodasCiudades();
+}
+
+function pintarTodasCiudades(){
+    noStroke();
+    fill(255, 0, 0);
+    textSize(14);
+
+    pintarMadrid();
+}
+
+function pintarMadrid(){
+    text(data[0].list[i].main.temp_max, 50, 80);
 }
 
 function pintarCuadricula(){
-    for(let i = 0; i < width; i+10){
-        line(i, 0, i, height);
+    textSize(10);
+    for(let x = 0; x < width; x=x+10){
+        if((x/10)%2==0){
+            text(x/10, x, 10);
+        }
+        line(x, 0, x, height);
     }
-    for(let i = 0; i < height; i+10){
-        line(0, i, width, i);
+    for(let y = 0; y < height; y=y+10){
+        if((y/10)%2==0){
+            text(y/10, 10, y);
+        }
+        line(0, y, width, y);
     }
 }
